ffda54ff2bf41d59a88c19f3e8c600544644f3cd
vinnn223 <100808434+vinnn223@users.noreply.github.com>
2024-06-02 20:16:36 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,8 +3,6 @@ Config = {}
 -- Define roles that are allowed to use the announcestaff command
 Config.AllowedRoles = {
     'admin',
-    'superadmin',
-    'mod
 }
 
 -- Define the command name for announcestaff
